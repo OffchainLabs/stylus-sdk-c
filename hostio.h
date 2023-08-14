45c70019bd135a4bf809ba6cec0090ad10e6fb85
Joshua Colvin <jcolvin@offchainlabs.com>
2023-08-14 12:32:19 -0700

    Update stylus header to 0xEFF000

--- hostio.h
+++ hostio.h
@@ -141,7 +141,7 @@ VM_HOOK(contract_address) void contract_address(uint8_t * address);
  * Deploys a new contract using the init code provided, which the EVM executes to construct
  * the code of the newly deployed contract. The init code must be written in EVM bytecode, but
  * the code it deploys can be that of a Stylus program. The code returned will be treated as
- * WASM if it begins with the EOF-inspired header `0xEF000000`. Otherwise the code will be
+ * WASM if it begins with the EOF-inspired header `0xEFF000`. Otherwise the code will be
  * interpreted as that of a traditional EVM-style contract. See [`Deploying Stylus Programs`]
  * for more information on writing init code.
  * 
@@ -166,7 +166,7 @@ VM_HOOK(create1) void create1(
  * Deploys a new contract using the init code provided, which the EVM executes to construct
  * the code of the newly deployed contract. The init code must be written in EVM bytecode, but
  * the code it deploys can be that of a Stylus program. The code returned will be treated as
- * WASM if it begins with the EOF-inspired header `0xEF000000`. Otherwise the code will be
+ * WASM if it begins with the EOF-inspired header `0xEFF000`. Otherwise the code will be
  * interpreted as that of a traditional EVM-style contract. See [`Deploying Stylus Programs`]
  * for more information on writing init code.
  * 
